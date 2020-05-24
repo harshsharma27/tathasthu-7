b2f16fc9d7f3dae914bf81d0ceff9adaa1cd7e41
harshsharma27 <53096800+harshsharma27@users.noreply.github.com>
2020-05-24 08:23:55 +0530

    Update program3.py

--- day 2/program3.py
+++ day 2/program3.py
@@ -1,29 +1,5 @@
-n = int(input("Enter the number: "))
-k=n
-h = n//2
-
-for i in range(0,h):
-    for m in range(0,i):
-        print(" ",end="")
-
-    print("*",end="")
-    for j in range(i+1,k):
-        if(j==k-1):
-            print("*",end="")
-        else:
-            print(" ",end="")
-    k = k-1
-    print("\n")
-k=h
-for i in range(h,n):
-    for m in range(i+1,n):
-        print(" ",end="")
-
-    print("*",end="")
-    for j in range():
-        if(j==k-h):
-            print("*",end="")
-        else:
-            print(" ",end="")
-    k = k+2
-    print("\n")
+n = int(input ("Enter the number: ")) 
+for i in range(1,n+1):
+	print( " "*i+"*"+"  "*(n-i)+"*")
+for i in range(1,n+1):
+	print(" "*(n-i+1)+"*"+" "*(2*i-2)+"*")
